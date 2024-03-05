037bc9431c745efcc49f598038ff5c2f46768bb7
Estifanos Taye <estifanostaye11@gmail.com>
2024-03-05 16:33:06 +0300

    Exersice 3 Asynchronus-programming

--- LAB-1-Asynchronus-programming.dart
+++ LAB-1-Asynchronus-programming.dart
@@ -5,7 +5,7 @@ void main() async {
   Future<int> getNumber =
       Future.delayed(Duration(seconds: 2), () => Random().nextInt(10) + 1);
 
-  int number = await getNumber;
-
-  print(number);
+  getNumber.then((int number) {
+    print(number);
+  });
 }
