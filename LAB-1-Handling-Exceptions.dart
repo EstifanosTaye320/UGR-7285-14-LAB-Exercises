29d0d20b7f824a336ddde0a3ad557cb2448e7b95
Estifanos Taye <estifanostaye11@gmail.com>
2024-03-05 16:46:02 +0300

    Exersice 2 Handling-Exceptions

--- LAB-1-Handling-Exceptions.dart
+++ LAB-1-Handling-Exceptions.dart
@@ -1,11 +1,23 @@
+import 'dart:math';
+
 void myFonnyFunction() {
-  throw Exception('some exception');
+  int someRand = Random().nextInt(3) + 1;
+  switch (someRand) {
+    case 1:
+      throw Exception('some exception');
+    case 2:
+      throw FormatException('formatException');
+  }
 }
 
 void main() {
   try {
     myFonnyFunction();
   } catch (e) {
-    print(e);
+    if (e is FormatException) {
+      print('spacial Exception' + e.toString());
+    } else {
+      print('other Exception' + e.toString());
+    }
   }
 }
