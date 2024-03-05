6a67bd8e2e0a5990278016cf731477cc62ea861f
Estifanos Taye <estifanostaye11@gmail.com>
2024-03-05 16:18:44 +0300

    Exersice 1 Classes and Objects

---
+++ LAB-1-Classes-And-Objects.dart
@@ -0,0 +1,13 @@
+class Car {
+  const Car(this.brand, this.model, this.year);
+  final String brand;
+  final String model;
+  final String year;
+}
+
+void main() {
+  Car fordCar = Car("Brand", 'model', 'year');
+  print(fordCar.brand);
+  print(fordCar.model);
+  print(fordCar.year);
+}
